73bee20de96bf143bc7f50ab2fd85243074abbf7
DESKTOP-4SFVT61\admin <huylpq@gmail.com>
2023-11-28 09:52:38 +0700

    create order

--- server/services/order.service.js
+++ server/services/order.service.js
@@ -1,6 +1,27 @@
 const db = require('../models')
 const {Op} = require('sequelize')
+const { v4: uuidv4 } = require('uuid');
 
 exports.createOrder = async(cartId) => {
-    
+    const Order = await db.Order;
+    const Cart = await db.Cart;
+
+    const cart = await Cart.findOne({
+        where: {
+            id: cartId
+        }
+    });
+
+    await Order.create({
+        id: uuidv4(),
+        userId: cart.userId,
+        cartId: cart.id,
+        totalAmount: cart.totalPrice,
+        status: 1
+    })
+
+    return {
+        status: 200,
+        message: "Order success"
+    }
 };
--- server/services/product.service.js
+++ server/services/product.service.js
@@ -24,7 +24,7 @@ exports.createProduct = async(userId, dataProd) => {
     })
     return {
         status: 200,
-        message: "create product categories successfully"
+        message: "create product successfully"
     }
 };
 
